61d47c3f622a3e44acf795f4220f87b6c0da54f7
Salil Chhetri <salil@shiftrock.com>
2019-09-26 19:52:42 +0530

    Updates

---
+++ precache-manifest.cdf2159b12728e26cdfd4dbd7317e5f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0450f45efde356b95de51d7d0222c2f2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88e043fb0c2b773157e4",
+    "url": "/monsters-rolodex/static/css/main.87abb0b7.chunk.css"
+  },
+  {
+    "revision": "deda4bc87b640a0fcefa",
+    "url": "/monsters-rolodex/static/js/2.b12b236f.chunk.js"
+  },
+  {
+    "revision": "88e043fb0c2b773157e4",
+    "url": "/monsters-rolodex/static/js/main.4a2bfb04.chunk.js"
+  },
+  {
+    "revision": "e69686689e4d86b7a3ef",
+    "url": "/monsters-rolodex/static/js/runtime-main.4f730ae0.js"
+  }
+]);
